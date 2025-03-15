dc04f82483f007a313fe60c14ca6bc5fe9bf1330
Shriram Tiwari <shriramt.124@gmail.com>
2025-03-15 21:43:58 +0530

    "updated"

--- app/component/hero.js
+++ app/component/hero.js
@@ -2,7 +2,7 @@ import { HeroGeometric } from "@/components/ui/shape-landing-hero"
 
 
 export default function Hero() {
-    return <HeroGeometric badge="Kokonut UI"
+    return <HeroGeometric badge="shriram tiwari"
         title1="Code. Create. Innovate"
         title2="Shriram Tiwari"
        
